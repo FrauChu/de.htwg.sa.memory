9194bd3bf586330b4a86da172eb60ae9ae25a0f3
FrauChu <sandra-b@outlook.de>
2016-06-26 16:02:28 +0200

    added files

---
+++ src/de/htwg/memory/logic/IController.java
@@ -0,0 +1,59 @@
+package de.htwg.memory.logic;
+
+import de.htwg.memory.entities.Board;
+import de.htwg.memory.entities.IMemoryCard;
+import de.htwg.memory.entities.MemoryCardEventListener;
+
+import java.io.File;
+
+/**
+ * Created by simon on 21.06.16.
+ */
+public interface IController extends MemoryCardEventListener {
+    void loadBoard(File gameFile);
+
+    void loadStaticBoard(int width, int height);
+
+    void replaceBoard(Board newBoard);
+
+    void resetGame();
+
+    void hideWrongMatch();
+
+    @Override
+    void picked(IMemoryCard mc);
+
+    boolean pickCard(int x, int y);
+
+    boolean processResult(Board.PickResult result);
+
+    Board getBoard();
+
+    int getPlayerCount();
+
+    int getCurrentPlayer();
+
+    void setPlayerCount(int players);
+
+    int getRoundNumber();
+
+    int getPlayerMatches(int playerId);
+
+    void fireWin();
+
+    void fireMatchMade();
+
+    void fireNoMatchMade();
+
+    void fireBoardNeedsReload();
+
+    void fireBoardChanged();
+
+    void firePlayerCountChanged(int players);
+
+    void fireGameReset();
+
+    void addListener(UiEventListener listener);
+
+    void removeListener(UiEventListener listener);
+}
---
+++ src/de/htwg/memory/main/MainModule.java
@@ -0,0 +1,16 @@
+package de.htwg.memory.main;
+
+import com.google.inject.AbstractModule;
+import de.htwg.memory.logic.Controller;
+import de.htwg.memory.logic.IController;
+import de.htwg.memory.persistence.IHighscoreDAO;
+
+
+public class MainModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(IController.class).to(Controller.class);
+		bind(IHighscoreDAO.class).to(de.htwg.memory.persistence.couchdb.HighscoreCouchdbDAO.class);
+//      bind(IHighscoreDAO.class).to(de.htwg.memory.persistence.hibernate.HighscoreHibernateDAO.class);
+    }
+}
